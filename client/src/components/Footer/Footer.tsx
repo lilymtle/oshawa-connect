f9c480e4ee903e9a333bd07a96651292c287bae4
Lily Le <lmlt8180@gmail.com>
2025-06-11 18:12:50 -0400

    update footer
    - create temporary skeleton for footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,8 +3,52 @@ import "./Footer.scss"
 
 export default function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className="footer">
+            <div className="footer__nav">
+                <div className="footer__nav-wrapper">
+                    <p className="footer__nav-label">
+                        Oshawa Connect
+                    </p>
+
+                    <ul className="footer__nav-list">
+                        <li className="footer__nav-list-item">
+                            Home
+                        </li>
+
+                        <li className="footer__nav-list-item">
+                            About
+                        </li>
+
+                        <li className="footer__nav-list-item">
+                            FAQs
+                        </li>
+
+                        <li className="footer__nav-list-item">
+                            Contact
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="footer__nav-wrapper">
+                    <p className="footer__nav-label">
+                        Explore Oshawa
+                    </p>
+
+                    <ul className="footer__nav-list">
+                        <li className="footer__nav-list-item">
+                            Events
+                        </li>
+
+                        <li className="footer__nav-list-item">
+                            Community Resources
+                        </li>
+
+                        <li className="footer__nav-list-item">
+                            Local Businesses
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     );
 }
