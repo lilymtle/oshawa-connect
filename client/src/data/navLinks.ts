c7a42644ff35fe0c7ed48cb2bc267cdc3fdb2920
Lily Le <lmlt8180@gmail.com>
2025-06-11 19:28:52 -0400

    create nav links file

---
+++ client/src/data/navLinks.ts
@@ -0,0 +1,14 @@
+interface NavLinkProps {
+    name: string;
+    path: string;
+}
+
+export const navLinks: NavLinkProps[] = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/" },
+    { name: "FAQs", path: "/" },
+    { name: "Contact", path: "/" },
+    { name: "Events", path: "/" },
+    { name: "Community Resources", path: "/" },
+    { name: "Local Businesses", path: "/" }
+]
