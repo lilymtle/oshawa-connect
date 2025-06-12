d07859aee529462e89db8e0a6c0d3d7477453a98
Lily Le <lmlt8180@gmail.com>
2025-06-11 20:20:03 -0400

    update header
    - remove unused imports
    - import mui close icon
    - add closing state for drawer animation
    - create handleClose function for drawer
    - update nav class name to include conditional class naming

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Divider, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import "./Header.scss"
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from "react";
@@ -6,14 +6,25 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { navLinks } from "../../data/navLinks";
 
+import CloseIcon from '@mui/icons-material/Close';
+
 export default function Header() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isClosing, setIsClosing] = useState<boolean>(false);
 
     const headerNavLinks = () => {
         const homeAboutLinks = navLinks.slice(0, 2);
         const exploreOshawaLinks = navLinks.slice(4);
         return homeAboutLinks.concat(exploreOshawaLinks);
-    }
+    };
+
+    const handleClose = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            setIsOpen(false);
+            setIsClosing(false);
+        }, 500); 
+    };
 
     return (
         <header className="header">
@@ -34,15 +45,25 @@ export default function Header() {
 
             { isOpen && 
                 <nav 
-                    className="header__mobile-nav"
+                    className={`header__mobile-nav ${isOpen ? (isClosing ? "close" : "open") : "hidden"}`}
                 >
-                    <ul className="header__mobile-nav-list">
-                        {headerNavLinks().map((link) => (
-                            <li className="header__mobile-nav-list-item">
-                                <Link to={link.path}>{link.name}</Link>
-                            </li>
-                        ))}
-                    </ul>
+                    <div className="header__mobile-nav-close">
+                        <IconButton onClick={() => handleClose()}>
+                            <CloseIcon />
+                        </IconButton>
+                    </div>
+                    
+                    <div className="header__mobile-nav-wrapper">
+                        <ul className="header__mobile-nav-list">
+                            {headerNavLinks().map((link) => (
+                                <li className="header__mobile-nav-list-item">
+                                    <Link to={link.path} onClick={() => setIsOpen(!isOpen)}>
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </nav> 
             }
         </header>
