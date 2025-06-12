c6ba5da0667f3923e32c144a539df0328376b118
Lily Le <lmlt8180@gmail.com>
2025-06-11 21:26:13 -0400

    update homepage
    - add discover section with placeholder text

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,55 @@
 import "./HomePage.scss"
 
+import EventIcon from '@mui/icons-material/Event';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import PeopleIcon from '@mui/icons-material/People';
+
 export default function HomePage() {
   return (
-    <p>
-      Home page
-    </p>
+    <section className="home">
+      <section className="home__discover">
+        <h2 className="home__subheading">Discover Oshawa</h2>
+
+        <div className="home__discover-wrapper">
+          <ul className="home__discover-list">
+            <li className="home__discover-list-item">
+              < EventIcon />
+
+              <p className="home__discover-label">
+                Events
+              </p>
+
+              <p className="home__discover-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+              </p>
+            </li>
+
+            <li className="home__discover-list-item">
+              <LocalDiningIcon />
+
+              <p className="home__discover-label">
+                Local Businesses
+              </p>
+
+              <p className="home__discover-text">
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
+              </p>
+            </li>
+
+            <li className="home__discover-list-item">
+              <PeopleIcon />
+
+              <p className="home__discover-label">
+                Community Resources
+              </p>
+
+              <p className="home__discover-text">                
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </section>
   )
 }
