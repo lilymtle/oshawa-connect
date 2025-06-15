8e43bd8be557b3efaf6cc9473748f16f13321caf
Lily Le <lmlt8180@gmail.com>
2025-06-15 19:44:42 -0400

    update image path

--- client/src/data/upcomingEvents.ts
+++ client/src/data/upcomingEvents.ts
@@ -1,29 +1,29 @@
 interface UpcomingEventsProps {
     id: number;
     date: string;
-    eventName: string;
+    name: string;
     image: string;
 }
 
-const imagePath = "/assets/images/upcoming-events-image";
+const imagePath = "/assets/images/upcoming-event-image";
 
 export const upcomingEvents: UpcomingEventsProps[] = [
     {
         id: 1,
         date: "August 30, 2025",
-        eventName: "SixQuad Summer League",
+        name: "SixQuad Summer League",
         image: `${imagePath}-1.png`
     },
     {
         id: 2,
         date: "Sep 19, 2025",
-        eventName: "Skydiggers",
+        name: "Skydiggers",
         image: `${imagePath}-2.png`
     },
     {
         id: 3,
         date: "Sep 19 - 20, 2025",
-        eventName: "Convergence Festival",
+        name: "Convergence Festival",
         image: `${imagePath}-3.png`
     }
 ]
