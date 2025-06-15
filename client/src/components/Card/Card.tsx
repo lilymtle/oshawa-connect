0bfb1a2156b1ba38297161666f5132894a6933aa
Lily Le <lmlt8180@gmail.com>
2025-06-15 19:44:49 -0400

    add style prop to card component

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -2,12 +2,13 @@ import "./Card.scss"
 
 interface CardProps {
     className?: string;
+    style?: React.CSSProperties;
     children: React.ReactNode; 
 };
 
-export default function Card({ className, children }: CardProps) {
+export default function Card({ className, style, children }: CardProps) {
     return(
-        <div className={`card ${className}`}>
+        <div className={`card ${className}`} style={style}>
             {children}
         </div>
     );
