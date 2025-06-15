28dcc17cc42efccbf13e95023827e6fa64a65ab2
Lily Le <lmlt8180@gmail.com>
2025-06-15 17:30:30 -0400

    create card props

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,9 +1,14 @@
 import "./Card.scss"
 
-export default function Card() {
+interface CardProps {
+    className?: string;
+    children: React.ReactNode; 
+};
+
+export default function Card({ className, children }: CardProps) {
     return(
-        <div className="card">
-            Card
+        <div className={`card ${className}`}>
+            {children}
         </div>
     );
 }
