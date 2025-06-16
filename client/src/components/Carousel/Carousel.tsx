f37bacc526329365fd2bf889130fc7a0b5d395ec
Lily Le <lmlt8180@gmail.com>
2025-06-15 21:22:51 -0400

    update carousel
    - add carousel props
    - set up scrolling logic
    - import useRef and useState

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,9 +1,53 @@
+import React, { useRef, useState } from "react";
 import "./Carousel.scss"
 
-export default function Carousel() {
+interface CarouselProps {
+    className: string;
+    children: React.ReactNode;
+}
+
+export default function Carousel({ className, children }: CarouselProps) {
+    const carouselRef = useRef<HTMLUListElement>(null);
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const childrenArr = React.Children.toArray(children);
+    const totalSlides = childrenArr.length;
+
+    const scrollToIndex = (index: number) => {
+        const scrollAmount = 300;
+        carouselRef.current?.scrollTo({
+            left: index * scrollAmount,
+            behavior: "smooth"
+        });
+    };
+
+    const handleLeft = () => {
+        const newIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+        setCurrentIndex(newIndex);
+        scrollToIndex(newIndex);
+    };
+
+    const handleRight = () => {
+        const newIndex = (currentIndex + 1) % totalSlides;
+        setCurrentIndex(newIndex);
+        scrollToIndex(newIndex);
+    }
+
     return (
-        <div className="carousel">
-            Placeholder
+        <div className={`carousel ${className}`}>
+            <button className="carousel__arrow carousel__arrow--left" onClick={handleLeft}>
+                <img className="carousel__arrow-icon" src="/assets/icons/arrow-left.svg" alt="Left Arrow" />
+            </button>
+
+            <div className="carousel__track-wrapper">
+                <ul className="carousel__track" ref={carouselRef}>
+                    {children}
+                </ul>
+            </div>
+
+            <button className="carousel__arrow carousel__arrow--right" onClick={handleRight}>
+                <img className="carousel__arrow-icon" src="/assets/icons/arrow-right.svg" alt="Right Arrow" />
+            </button>
         </div>
     )
 }
