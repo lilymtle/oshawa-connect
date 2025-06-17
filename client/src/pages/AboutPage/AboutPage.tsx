555ee39be6000aaf499327d0e7584cd6b77c6d35
Lily Le <lmlt8180@gmail.com>
2025-06-16 20:50:16 -0400

    add sections and headings to about page

--- client/src/pages/AboutPage/AboutPage.tsx
+++ client/src/pages/AboutPage/AboutPage.tsx
@@ -3,7 +3,26 @@ import "./AboutPage.scss"
 export default function AboutPage() {
     return (
         <section className="about">
-            About Page
+            <div className="about__wrapper">
+                <h2 className="section-heading section-heading--gold">Oshawa Connect</h2>
+                <h3 className="content-heading content-heading--white">How It Started</h3>
+            </div>
+
+            <section className="about__how-it-works">
+                <h2 className="section-heading">How It Works</h2>
+            </section>
+
+            <section className="about__whats-next">
+                <h2 className="section-heading section-heading--white">What's Next</h2>
+
+                <div className="about__whats-next-feedback">
+                    <h3 className="content-heading content-heading--onyx">Your City, Your Voice</h3>
+                </div>
+            </section>
+
+            <section className="about__behind-the-screen">
+                <h2 className="section-heading">Behind the Screen</h2>
+            </section>
         </section>
     );
 }
