b1e589cfcc6debfa88588da666f2641d046ddb06
Lily Le <lmlt8180@gmail.com>
2025-07-18 11:48:01 -0400

    create faqs page boiler plate

---
+++ client/src/pages/FAQsPage/FAQsPage.tsx
@@ -0,0 +1,11 @@
+import "./FAQsPage.scss"
+
+export function FAQsPage() {
+    return (
+        <section className="faqs">
+            <div className="faqs__wrapper">
+                <h2 className="section-heading">Let's Get Started</h2>
+            </div>
+        </section>
+    )
+}
