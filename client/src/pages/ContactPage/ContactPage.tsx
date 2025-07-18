214121b38473793910489832821b0ab31e6cb4a6
Lily Le <lmlt8180@gmail.com>
2025-07-18 15:40:38 -0400

    create contact page
    - add heading and description
    - create basic form structure

---
+++ client/src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,84 @@
+import Button from "../../components/Button/Button";
+import "./ContactPage.scss";
+
+export default function ContactPage() {
+    return (
+        <section className="contact">
+            <div className="contact__wrapper">
+                <h2 className="section-heading">
+                    Share Your Thoughts
+                </h2>
+
+                <p className="contact__description">
+                    Have an idea, comment, or question? I’d love to hear from you — just use the form below, and I’ll get back to you as 
+                    soon as I can.
+                </p>
+            </div>
+
+            <div className="contact__form-container">
+                <form className="form">
+                    <div className="form__field">
+                        <label 
+                            htmlFor="name"
+                            className="form__label"
+                        >
+                            Name <span className="form__required" aria-hidden="true">(required)</span>
+                        </label>
+                        <input
+                            id="name"
+                            type="text"
+                            name="name"
+                            className="form__input"
+                            placeholder="Enter your name"
+                            required
+                            aria-required="true"
+                            autoComplete="name"
+                        />
+                    </div>
+
+                    <div className="form__field">
+                        <label 
+                            htmlFor="email"
+                            className="form__label"
+                        >
+                            Email <span className="form__required" aria-hidden="true">(required)</span>
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            className="form__input"
+                            placeholder="example@email.com"
+                            required
+                            aria-required="true"
+                            autoComplete="email"
+                        />
+                    </div>
+
+                    <div className="form__field">
+                        <label 
+                            htmlFor="message"
+                            className="form__label"
+                        >
+                            Message <span className="form__required" aria-hidden="true">(required)</span>
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="form__input form__input--textarea"
+                            placeholder="Enter your message"
+                            required
+                            aria-required="true"
+                        />
+                    </div>
+
+                    <Button
+                        className="form__button"
+                        variant="primary"
+                        label="Send"
+                    />
+                </form>
+            </div>
+        </section>
+    )
+}
