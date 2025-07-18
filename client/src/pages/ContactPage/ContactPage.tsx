296d596060c25ab4a5230ceb174fb832b7aeb9c4
Lily Le <lmlt8180@gmail.com>
2025-07-18 17:31:05 -0400

    update contact page
    - import formspree
    - add thank you message upon successful form submission
    - update form to include action, method, and onSubmit attributes
    - enclose form and thank you message in a div wrapper
    - add disabled attribute to button

--- client/src/pages/ContactPage/ContactPage.tsx
+++ client/src/pages/ContactPage/ContactPage.tsx
@@ -1,7 +1,11 @@
+import { useForm } from "@formspree/react";
 import Button from "../../components/Button/Button";
 import "./ContactPage.scss";
 
 export default function ContactPage() {
+    const formspreeID = import.meta.env.VITE_FORMSPREE_ID;
+    const [state, handleSubmit] = useForm(formspreeID);
+
     return (
         <section className="contact">
             <div className="contact__wrapper">
@@ -22,69 +26,81 @@ export default function ContactPage() {
                     <div className="circle circle--green"></div>
                 </div>
 
-                <form className="form">
-                    <div className="form__field">
-                        <label 
-                            htmlFor="name"
-                            className="form__label"
-                        >
-                            Name <span className="form__required" aria-hidden="true">(required)</span>
-                        </label>
-                        <input
-                            id="name"
-                            type="text"
-                            name="name"
-                            className="form__input"
-                            placeholder="Enter your name"
-                            required
-                            aria-required="true"
-                            autoComplete="name"
-                        />
-                    </div>
+                <div className="form__wrapper">
+                    {state.succeeded ? (
+                        <p className="form_success">Thank you for your message! ☺️</p> 
+                    ) : ""}
 
-                    <div className="form__field">
-                        <label 
-                            htmlFor="email"
-                            className="form__label"
-                        >
-                            Email <span className="form__required" aria-hidden="true">(required)</span>
-                        </label>
-                        <input
-                            id="email"
-                            type="email"
-                            name="email"
-                            className="form__input"
-                            placeholder="example@email.com"
-                            required
-                            aria-required="true"
-                            autoComplete="email"
-                        />
-                    </div>
+                    <form 
+                        className="form"
+                        action={`https://formspree.io/f/${formspreeID}`}
+                        method="POST"
+                        onSubmit={handleSubmit}
+                    >
+                        <div className="form__field">
+                            <label 
+                                htmlFor="name"
+                                className="form__label"
+                            >
+                                Name <span className="form__required" aria-hidden="true">(required)</span>
+                            </label>
+                            <input
+                                id="name"
+                                type="text"
+                                name="name"
+                                className="form__input"
+                                placeholder="Enter your name"
+                                required
+                                aria-required="true"
+                                autoComplete="name"
+                            />
+                        </div>
 
-                    <div className="form__field">
-                        <label 
-                            htmlFor="message"
-                            className="form__label"
-                        >
-                            Message <span className="form__required" aria-hidden="true">(required)</span>
-                        </label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            className="form__input form__input--textarea"
-                            placeholder="Enter your message"
-                            required
-                            aria-required="true"
-                        />
-                    </div>
+                        <div className="form__field">
+                            <label 
+                                htmlFor="email"
+                                className="form__label"
+                            >
+                                Email <span className="form__required" aria-hidden="true">(required)</span>
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                name="email"
+                                className="form__input"
+                                placeholder="example@email.com"
+                                required
+                                aria-required="true"
+                                autoComplete="email"
+                            />
+                        </div>
 
-                    <Button
-                        className="form__button"
-                        type="submit"
-                        variant="primary"
-                        label="Send"
-                    />
-                </form>
+                        <div className="form__field">
+                            <label 
+                                htmlFor="message"
+                                className="form__label"
+                            >
+                                Message <span className="form__required" aria-hidden="true">(required)</span>
+                            </label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                className="form__input form__input--textarea"
+                                placeholder="Enter your message"
+                                required
+                                aria-required="true"
+                            />
+                        </div>
+
+                        <Button
+                            className="form__button"
+                            type="submit"
+                            variant="primary"
+                            disabled={state.submitting}
+                            label="Send"
+                        />
+                    </form>
+                </div>
             </div>
         </section>
     )
