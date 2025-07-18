3e150cdb355ea69d86567ec70c2110d5be47d2c4
Lily Le <lmlt8180@gmail.com>
2025-07-18 12:42:17 -0400

    update faqs page
    - add faqs description
    - add faqs category cards

--- client/src/pages/FAQsPage/FAQsPage.tsx
+++ client/src/pages/FAQsPage/FAQsPage.tsx
@@ -1,10 +1,33 @@
-import "./FAQsPage.scss"
+import "./FAQsPage.scss";
+import { FAQsCard, FAQs } from "../../data/faqsData";
+import Card from "../../components/Card/Card";
 
 export function FAQsPage() {
     return (
         <section className="faqs">
             <div className="faqs__wrapper">
                 <h2 className="section-heading">Let's Get Started</h2>
+
+                <p className="faqs__description text">
+                    New to Oshawa Connect? Pick a category below to find answers to common questions, or browse all FAQs here.
+                </p>
+
+                <div className="faqs__cards">
+                    <ul className="faqs__list">
+                        {FAQsCard.map((card) => (
+                            <li className="faqs__item">
+                                <Card
+                                    key={card.id}
+                                    className="faqs__card"
+                                >
+                                    <h3 className="card__heading">
+                                        {card.category}
+                                    </h3>
+                                </Card>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </section>
     )
