76afd29f9bc803527af37fd98414592c26cf4646
Lily Le <lmlt8180@gmail.com>
2025-07-18 17:31:45 -0400

    add disabled attribute as prop

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -4,13 +4,14 @@ interface ButtonProps {
     className: string;
     type?: "button" | "submit"
     variant: "primary" | "secondary" | "tertiary";
+    disabled?: boolean;
     label: string;
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Button({ className, type="button", variant, label, onClick }: ButtonProps) {
+export default function Button({ className, type="button", variant, disabled, label, onClick }: ButtonProps) {
     return (
-        <button type={type} className={`${className} ${variant}`} onClick={onClick}>
+        <button type={type} className={`${className} ${variant}`} disabled={disabled} onClick={onClick}>
             {label}
         </button>
     );
