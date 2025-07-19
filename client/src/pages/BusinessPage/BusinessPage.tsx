e283e797fa536aab9f431eac9e21a9973015319a
Lily Le <lmlt8180@gmail.com>
2025-07-19 19:33:01 -0400

    create boiler plate for business page and scss file
    - add heading and leading text to business page
    - add padding to business section

---
+++ client/src/pages/BusinessPage/BusinessPage.tsx
@@ -0,0 +1,16 @@
+import "./BusinessPage.scss"
+
+export default function BusinessPage() {
+    return (
+        <section className="business">
+            <div className="business__wrapper">
+                <h2 className="section-heading">Support Local</h2>
+
+                <p className="business__description">
+                    Is there a local business missing and you would love to see here? 
+                    You can submit a recommendation by visiting this page. {/* add link later */}
+                </p>
+            </div>
+        </section>
+    )
+}
