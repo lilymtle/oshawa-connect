5201c473529b09d30068ccb835b47318876ac2cb
Lily Le <lmlt8180@gmail.com>
2025-07-19 21:36:29 -0400

    add emoji prop

--- client/src/components/Pill/Pill.tsx
+++ client/src/components/Pill/Pill.tsx
@@ -4,11 +4,13 @@ interface PillProps {
     id: number;
     category: string;
     style: React.CSSProperties;
+    emoji: string;
 }
 
-export default function Pill({ id, category, style }: PillProps) {
+export default function Pill({ id, category, style, emoji}: PillProps) {
     return (
         <div key={id} className="pill" style={style}>
+            <div className="pill__circle"><span className="pill__emoji">{emoji}</span></div>
             {category}
         </div>
     )
