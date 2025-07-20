ba371f6ebed906b717f1f2ab672e7546c8fb40bb
Lily Le <lmlt8180@gmail.com>
2025-07-19 22:19:31 -0400

    create category section component
    - component will be used for the events, businesses, and resources page with dynamic pills to reflect unique categories
    - move category styling from business page to category section scss

---
+++ client/src/components/CategorySection/CategorySection.tsx
@@ -0,0 +1,37 @@
+/* --- styling --- */
+import "./CategorySection.scss"
+
+/* --- react and react related imports --- */
+import { Link } from "react-router-dom"
+
+/* --- components --- */
+import Pill from "../Pill/Pill"
+
+/* --- data --- */
+import { businessCategories } from "../../data/categoriesList"
+
+export default function CategorySection() {
+    return (
+        <section className="category">
+            <div className="business__wrapper business__wrapper--category">
+                <h3 className="content-heading content-heading--onyx">Category</h3>
+                <Link className="category__link" to="/">
+                    See all
+                </Link>
+            </div>
+
+            <div className="category__wrapper">
+                <ul className="category__list">
+                    {businessCategories.map((category) => (
+                        <Pill
+                            id={category.id}
+                            category={category.category}
+                            style={{backgroundColor: `${category.backgroundColor}`}}
+                            emoji={category.emoji}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
