3d4d05ab4b5eed990e46f93ddc1ac13ff2b52de6
Lily Le <lmlt8180@gmail.com>
2025-07-21 18:59:51 -0400

    add onClick type property to pill

--- client/src/components/Pill/Pill.tsx
+++ client/src/components/Pill/Pill.tsx
@@ -7,11 +7,12 @@ interface PillProps {
     category: string;
     style: React.CSSProperties;
     emoji: string;
+    onClick: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-export default function Pill({ id, category, style, emoji}: PillProps) {
+export default function Pill({ id, category, style, emoji, onClick}: PillProps) {
     return (
-        <div key={id} className="pill" style={style}>
+        <div key={id} className="pill" style={style} onClick={onClick}>
             <div className="pill__circle"><span className="pill__emoji">{emoji}</span></div>
             {category}
         </div>
