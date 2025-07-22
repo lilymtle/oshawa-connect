2eaf9f5081925e5d5b84e22718d2f347618a58a0
Lily Le <lmlt8180@gmail.com>
2025-07-22 17:42:25 -0400

    create pagination component

---
+++ client/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./Pagination.scss";
+
+interface PaginationProps {
+    totalPages: number;
+    currentPage: number;
+    prevPage: () => void;
+    nextPage: () => void;
+}
+
+export default function Pagination({ totalPages, currentPage, prevPage, nextPage }: PaginationProps) {
+    const pageNumbers: number[] = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i)
+    };
+
+    return (
+        <div className="pagination">
+            <div 
+                className="pagination__wrapper"
+                onClick={prevPage}
+            >
+                <img
+                    className="pagination__arrow"
+                    src="/assets/icons/chevron-left.svg"
+                    alt="Left chevron arrow"
+                />
+
+                <p 
+                    className={`pagination__prev ${currentPage === 1 ? "disabled" : "" }`}
+                >
+                    Prev
+                </p>
+            </div>
+
+            <div className="pagination__pages">
+                {pageNumbers.map((page) => (
+                    <p className={`pagination__page-number ${currentPage ? "active" : ""}`}>
+                        {page}
+                    </p>
+                ))}
+            </div>
+
+            <div 
+                className="pagination__wrapper"
+                onClick={nextPage}
+            >
+                <p className="pagination__next">
+                    Next
+                </p>
+
+                <img
+                    className="pagination__arrow"
+                    src="/assets/icons/chevron-right.svg"
+                    alt="Right chevron arrow"
+                />
+            </div>
+        </div>
+    )
+}
