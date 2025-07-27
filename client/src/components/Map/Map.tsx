61a3af405b49e43dde0d4bad9256c4ffd7091527
Lily Le <lmlt8180@gmail.com>
2025-07-27 18:35:31 -0400

    add map type props, replace hardcoded values, update popup content

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -5,18 +5,35 @@ import "leaflet/dist/leaflet.css";
 /* --- leaflet imports -- */
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
-export const Map = () => {
-    const currentPosition: [number, number] = [51.505, -0.09]
+interface MapProps {
+    latLon: [number, number];
+    name: string;
+    street: string;
+    city: string;
+    province: string;
+    postalCode: string;
+}
+
+export default function Map({ latLon, name, street, city, province, postalCode }: MapProps) {
+    // const currentPosition: [number, number] = [43.6532, -79.3832]
 
     return (
-        <MapContainer center={currentPosition} zoom={15} scrollWheelZoom={false}>
+        <MapContainer center={latLon} zoom={15} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={latLon}>
                 <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                    <p className="location__name">{name}</p>
+                    <span className="location__address">
+                        <p className="location__street">{street}</p>
+                        <span className="location__wrapper">
+                            <p className="location__city">{city}</p>
+                            <p className="location__province">{province}</p>
+                        </span>
+                        <p className="location__postal-code">{postalCode}</p>
+                    </span>
                 </Popup>
             </Marker>
     </MapContainer>
