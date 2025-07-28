fd3a63bb7e2875abb512e08113110f9830baa943
Lily Le <lmlt8180@gmail.com>
2025-07-28 19:41:53 -0400

    update getlocation helper function
    - add if else statement to check if user browser supports geolocation
    - save user's location to a variable
    - add error handling message

--- client/src/utils/getLocation.ts
+++ client/src/utils/getLocation.ts
@@ -1,5 +1,13 @@
 export default function getLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude)  
-    })
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const currentLocation: [number, number] = [position.coords.latitude, position.coords.longitude];
+            console.log("Current location is:", currentLocation);
+        },
+        (error) => {
+            console.error("Error retrieving user location:", error);
+        })
+    } else {
+        console.error("Geolocation is not supported in this browser.");
+    }
 }
