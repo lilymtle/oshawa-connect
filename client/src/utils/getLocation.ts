c27529474dde06887182133c872532da35733f96
Lily Le <lmlt8180@gmail.com>
2025-07-28 21:24:40 -0400

    wrap if else statement as a Promise

--- client/src/utils/getLocation.ts
+++ client/src/utils/getLocation.ts
@@ -1,13 +1,14 @@
-export default function getLocation() {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const currentLocation: [number, number] = [position.coords.latitude, position.coords.longitude];
-            console.log("Current location is:", currentLocation);
-        },
-        (error) => {
-            console.error("Error retrieving user location:", error);
-        })
-    } else {
-        console.error("Geolocation is not supported in this browser.");
-    }
+export default function getLocation(): Promise<[number, number]> {
+    return new Promise((resolve, reject) => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                resolve([position.coords.latitude, position.coords.longitude]);
+            },
+            (error) => {
+                reject("Error retrieving user location:" + error.message);
+            })
+        } else {
+            reject("Geolocation is not supported in this browser.");
+        }
+    })
 }
