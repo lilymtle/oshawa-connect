043fb009015c6a9da403518b8c18b0820f4f418d
Lily Le <lmlt8180@gmail.com>
2025-08-27 17:30:54 -0400

    update category props and add community resource categories
    - replace union types with string type for emoji key
    - reformat businessCategories

--- client/src/data/categoriesList.ts
+++ client/src/data/categoriesList.ts
@@ -1,33 +1,55 @@
-interface CategoryProps {
+export interface CategoryProps {
     id: number;
     category: string;
-    emoji: "🏛️" | "🍽️" | "🎯" | "🛍️"
+    emoji: string;
     color: string;
 }
 
 export const businessCategories: CategoryProps[] = [
-        {
-            id: 1,
-            category: "Attractions",
-            emoji: "🏛️",
-            color: "#C3E7FA",
-        },
-        {
-            id: 2,
-            category: "Foods & Drinks",
-            emoji: "🍽️",
-            color: "#F4E5B5"
-        },
-        {
-            id: 3,
-            category: "Recreation",
-            emoji: "🎯",
-            color: "#B2E2CC"
-        },
-        {
-            id: 4,
-            category: "Retail",
-            emoji: "🛍️",
-            color: "#D6BADA"
-        }
-    ]
+    {
+        id: 1,
+        category: "Attractions",
+        emoji: "🏛️",
+        color: "#C3E7FA",
+    },
+    {
+        id: 2,
+        category: "Foods & Drinks",
+        emoji: "🍽️",
+        color: "#F4E5B5"
+    },
+    {
+        id: 3,
+        category: "Recreation",
+        emoji: "🎯",
+        color: "#B2E2CC"
+    },
+    {
+        id: 4,
+        category: "Retail",
+        emoji: "🛍️",
+        color: "#D6BADA"
+    }
+]
+
+export const communityResourceCategories: CategoryProps[] = [
+    {
+        id: 1,
+        category: "Employment",
+        emoji: "💼",
+        color: "#C3E7FA"
+    },
+    {
+        id: 2,
+        category: "Health",
+        emoji: "🌱",
+        color: "#F4E5B5"
+    },
+    {
+        id: 3,
+        category: "Housing",
+        emoji: "🏠",
+        color: "#B2E2CC"
+    }
+    
+]
