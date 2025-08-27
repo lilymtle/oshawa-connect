4894fd9f582fb614826713601a15f586c1c574b7
Lily Le <lmlt8180@gmail.com>
2025-08-27 17:33:40 -0400

    import community resource categories data and pass as prop to category section component
    - add page section heading and description

--- client/src/pages/CommunityResourcesPage/CommunityResourcesPage.tsx
+++ client/src/pages/CommunityResourcesPage/CommunityResourcesPage.tsx
@@ -1,10 +1,28 @@
+/* --- components --- */
+import CategorySection from "../../components/CategorySection/CategorySection";
+
+/* --- data --- */
+import { communityResourceCategories } from "../../data/categoriesList";
+
 /* --- styling --- */
 import "./CommunityResourcesPage.scss";
+
+
 
 export default function CommunityResourcesPage() {
     return (
         <section className="community-resources">
+            <div className="community-resources__wrapper">
+                <h2 className="section-heading">Find Support</h2>
 
+                <p className="community-resources__description">
+                    Find local support programs, services, and organizations that help keep our community connected and cared for.
+                </p>
+            </div>
+
+            <CategorySection
+                categories={communityResourceCategories}
+            />
         </section>
     )
 }
